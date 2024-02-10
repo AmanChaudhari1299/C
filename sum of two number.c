4c0b3642ede9213923d5852c861806d9e717985e
“[Aman <[chaudhariaman031@gmail.com]>
2024-02-10 16:38:19 +0530

    sum of two numbers

---
+++ sum of two number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int a;
+    printf("enter the value of a:");
+    scanf("%d",&a);
+    int b;
+    printf("enter the value of b:");
+    scanf("%d",&b);
+    int sum=a+b;
+    printf("sum=%d",sum);
+    return 0;
+    
+}
